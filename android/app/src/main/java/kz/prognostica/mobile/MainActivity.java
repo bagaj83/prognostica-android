4d0189b07e85e789b381ddc4a836368f93367790
bagaj83 <bagaj83@gmail.com>
2026-09-16 11:27:11 +0500

    Add Android activity

---
+++ android/app/src/main/java/kz/prognostica/mobile/MainActivity.java
@@ -0,0 +1,3 @@
+package kz.prognostica.mobile;
+import android.app.Activity;import android.os.Bundle;import android.webkit.WebSettings;import android.webkit.WebView;import android.webkit.WebViewClient;
+public class MainActivity extends Activity { private WebView webView; @Override protected void onCreate(Bundle b){super.onCreate(b);webView=new WebView(this);setContentView(webView);WebSettings s=webView.getSettings();s.setJavaScriptEnabled(true);s.setDomStorageEnabled(true);s.setAllowFileAccess(true);s.setAllowContentAccess(true);webView.setWebViewClient(new WebViewClient());webView.loadUrl("file:///android_asset/www/index.html");} @Override public void onBackPressed(){if(webView!=null&&webView.canGoBack())webView.goBack();else super.onBackPressed();}}
